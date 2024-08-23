67df9e9030f7983ecddfc1becaf6f9608486fdbf
allu0786ansari <allu456654ansari@gmail.com>
2024-08-23 23:45:46 +0530

    mysql database added

--- OneDrive/Desktop/Project/DemoProject/settings.py
+++ OneDrive/Desktop/Project/DemoProject/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'DemoProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'finance_assist',
+        'USER': 'root',
+        'PASSWORD': 'A6VLT@$cuDYA.4n',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
     }
 }
 
@@ -100,6 +104,8 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
@@ -121,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
